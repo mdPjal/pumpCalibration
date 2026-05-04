ad4010c1318856f6028dd195531fa204320ad308
mdPjal <mfba891024@gmail.com>
2026-05-04 14:32:43 +0800

    Update pumpCal.py

--- pumpCal.py
+++ pumpCal.py
@@ -10,12 +10,16 @@ def home():
 def health():
   return "OK"
 
+MAINTENANCE = false
 @app.route("/calculation", methods = ["GET"])
 def calculation ():
- vol = float(request.args.get("vol"))
- time1 = float(request.args.get("time1"))
- time2 = float(request.args.get("time2"))
- time3 = float(request.args.get("time3"))
- avg = (time1 + time2 + time3) / 3
- result = ((vol / 1000) * 3600) / avg
- return str(round(result,1))
+  if MAINTENANCE:
+    return str("Service Unavailable")
+    
+  vol = float(request.args.get("vol"))
+  time1 = float(request.args.get("time1"))
+  time2 = float(request.args.get("time2"))
+  time3 = float(request.args.get("time3"))
+  avg = (time1 + time2 + time3) / 3
+  result = ((vol / 1000) * 3600) / avg
+  return str(round(result,1))

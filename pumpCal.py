15939db58891c17d0259d4646cfb16ae93f1e614
mdPjal <mfba891024@gmail.com>
2026-05-04 11:50:29 +0800

    Create pumpCal.py

---
+++ pumpCal.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+@app.get("/")
+def home():
+  return "Server Is Alive"
+
+@app.get("/health")
+def health():
+  return "OK"
+
+@app.get("/calculation")
+def calculation ():
+ vol = float(request.args.get("vol"))
+ time1 = float(request.args.get("time1"))
+ time2 = float(request.args.get("time2"))
+ time3 = float(request.args.get("time3"))
+ avg = (time1 + time2 + time3) / 3
+ result = ((vol / 1000) * 3600) / avg
+ return round(result,1)

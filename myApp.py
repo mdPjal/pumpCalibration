d4c923c3282d1563b9c748c7d94cd9548c18ca4c
mdPjal <mfba891024@gmail.com>
2026-05-12 11:17:32 +0800

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -12,7 +12,7 @@ def home():
 def health():
   return "OK"
 
-#Pump Calibration
+#Pump Calibration to Jaini
 @app.route("/calculation", methods = ["GET"])
 def calculation ():
   if MAINTENANCE:
@@ -26,58 +26,60 @@ def calculation ():
   result = ((vol / 1000) * 3600) / avg
   return str(round(result,1))
 
-#Simple math
-@app.get("/add")
-def add():
-  if MAINTENANCE:
-    return str("Service Unavailable!")
-    
-  a = float(request.args.get("a"))
-  b = float(request.args.get("b"))
-  result = a + b
-  return str(round(result, 1))
-    
-@app.get("/subtraction")
-def subtraction():
+#Complete Pump Calibration
+@app.get("/pumpCalibration")
+def pumpCalibration():
   if MAINTENANCE:
     return str("Service Unavailable!")
     
   key = str(request.args.get("key"))
-
   if key != "VIP120":
-      return "Access Denied!"
+    return "Access Denied!"
+    
+  vol = float(request.args.get("vol"))
+    
+  time1 = float(request.args.get("time1"))
+  time2 = float(request.args.get("time2"))
+  time3 = float(request.args.get("time3"))
+  avg1 = (time1 + time2 + time3) / 3
+  result1 = ((vol / 1000) * 3600) / avg1
 
-  a = float(request.args.get("a"))
-  b = float(request.args.get("b"))
-  result = a - b
-  return str(round(result, 1))
+  time4 = float(request.args.get("time4"))
+  time5 = float(request.args.get("time5"))
+  time6 = float(request.args.get("time6"))
+  avg2 = (time4 + time5 + time6) / 3
+  result2 =((vol / 1000) * 3600) / avg2
 
-@app.get("/multiply")
-def multiply():
+  time7 = float(request.args.get("time7"))
+  time8 = float(request.args.get("time8"))
+  time9 = float(request.args.get("time9"))
+  avg3 = (time7 + time8 + time9) / 3
+  result3 =((vol / 1000) * 3600) / avg3
+
+  time10 = float(request.args.get("time10"))
+  time11 = float(request.args.get("time11"))
+  time12 = float(request.args.get("time12"))
+  avg4 = (time10 + time11 + time12) / 3
+  result4 =((vol / 1000) * 3600) / avg4
+
+  time13 = float(request.args.get("time13"))
+  time14 = float(request.args.get("time14"))
+  time15 = float(request.args.get("time15"))
+  avg5 = (time13 + time14 + time15) / 3
+  result5 =((vol / 1000) * 3600) / avg5
+     
+  return f"{str(round(result1,1))}, {str(round(result2,1))}, {str(round(result3,1))}, {str(round(result4,1))}, {str(round(result5,1))}"
+
+@app.get("/pumpVerification")
+def pumpVerification():
   if MAINTENANCE:
     return str("Service Unavailable!")
     
   key = str(request.args.get("key"))
-
   if key != "VIP121":
-      return "Access Denied!"
-        
-  a = float(request.args.get("a"))
-  b = float(request.args.get("b"))
-  result = a * b
-  return str(round(result, 1))
-
-@app.get("/divide")
-def divide():
-  if MAINTENANCE:
-    return str("Service Unavailable!")
+    return "Access Denied!"
     
-  key = str(request.args.get("key"))
-
-  if key != "VIP122":
-      return "Access Denied!"
-        
-  a = float(request.args.get("a"))
-  b = float(request.args.get("b"))
-  result = a / b
-  return str(round(result, 1))
+  time = float(request.args.get("time"))
+  vol = float(request.args.get("vol"))
+  result = ((vol / 1000) * 3600) / time
+  return str(round(result,1))
